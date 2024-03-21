a34912727787c2869e69c82ec800cbd81704de9f
student-glb <premsagar1987@rediffmail.com>
2024-03-21 23:53:27 +0530

    add and sub method added

--- a.c
+++ a.c
@@ -7,13 +7,23 @@ int fact(int n)
     return n*fact(n-1);
 }
 
+int add(int a, int b)
+{
+    return a+b;
+}
+
+int sub(int a,int b)
+{return a-b;}
 
 
 
 
 int main()
 {
-int n=5;
-printf("fact of %d = %d",n, fact(n));
+int n=5,a=10,b=20;
+printf("\nfact of %d = %d",n, fact(n));
+printf("\nsum of %d & %d = %d",a,b,add(a,b));
+printf("\nsub of %d & %d = %d",a,b, sub(a,b));
+
 return 0;
 }
